35363329df7d5c9021309b0429f6a9eca9305853
DanielTheSisyphean <dasatatz@hotmail.com>
2025-06-01 17:42:58 -0400

    test

--- src/server.js
+++ src/server.js
@@ -1,5 +1,18 @@
-const { PORT = 5000 } = process.env;
+const express = require('express');
+const cors = require('cors');
 
-const app = require("./app");
-const listener = () => console.log(`Listening on Port ${PORT}!`);
-app.listen(PORT, listener);
+const app = express();
+const router = express.Router();
+const PORT = process.env.PORT || 5001;
+
+router.get('/', cors(), (req, res) => {
+  res.json({ message: 'Hello Render!' });
+});
+
+app.use('/', router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT} `);
+});
+
+module.exports = app;
